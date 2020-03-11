e0071cf1efff17b7dc28ea55ac56161e4cf9bcd6
Girish Pathak <girish6j87@gmail.com>
2020-03-11 17:05:52 +0530

    Added code for ppmtool-react-client

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 export default class Dashboard extends Component {
   render() {
-    return <h1>welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>welcome to the Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
